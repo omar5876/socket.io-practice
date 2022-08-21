df4a38af50921984c139045a42d7bf0f3bfa606b
unknown <omarramirez5876@gmail.com>
2022-08-21 15:25:42 -0500

    practice 4

--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,36 +1,43 @@
 const socket = io();
 
-const circle = document.querySelector("#circle");
-
-const drawCircle = (position) => {
-    circle.style.top = position.top;
-    circle.style.left = position.left;
-}
-
-const drag = e => {
-   
-
-    const position = {
-        top: e.clientY + "px",
-        left: e.clientX + "px"
-    }
-
-    drawCircle(position)
-
-    socket.emit("circle position", position)
+const connectRoom1 = document.querySelector("#connectRoom1")
+const connectRoom2 = document.querySelector("#connectRoom2")
+const connectRoom3 = document.querySelector("#connectRoom3")
 
 
-}
-
-document.addEventListener("mousedown", e => {
-    document.addEventListener("mousemove", drag)
+connectRoom1.addEventListener("click", () => {
+    socket.emit("connect to room", "room1")
 })
 
-document.addEventListener("mouseup", e => {
-    document.removeEventListener("mousemove", drag)
+connectRoom2.addEventListener("click", () => {
+    socket.emit("connect to room", "room2")
+})
+
+connectRoom3.addEventListener("click", () => {
+    socket.emit("connect to room", "room3")
 })
 
 
-socket.on("move circle", position => {
-    drawCircle(position)
+//enviar mensaje
+
+
+const sendMessage = document.querySelector("#sendMessage");
+
+sendMessage.addEventListener("click", () => {
+    const message = prompt("Escribe tu mensaje")
+
+    socket.emit("message", message)
+})
+
+
+//Recibir el evento del mensaje
+
+socket.on("send message", data => {
+    const {room} = data;
+    const {message} = data;
+
+    const li = document.createElement("li");
+    li.textContent = message;
+
+    document.querySelector(`#${room}`).append(li);
 })
