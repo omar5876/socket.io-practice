f2b8c2d6aa9b7370b8fbe910662ee9dd03410c70
unknown <omarramirez5876@gmail.com>
2022-08-22 18:30:03 -0500

    practice 9

--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,13 +1,36 @@
-const socket = io({
-    auth: {
-        token: "MR.SATAN"
+const socket = io();
+
+const circle = document.querySelector("#circle");
+
+const drawCircle = (position) => {
+    circle.style.top = position.top;
+    circle.style.left = position.left;
+}
+
+const drag = e => {
+   
+
+    const position = {
+        top: e.clientY + "px",
+        left: e.clientX + "px"
     }
-});
 
-//en caso de error en el middleware
+    drawCircle(position)
 
-socket.on("connect_error", err => {
-    console.log("Error de conexion :(")
-    console.log(err.message)
-    console.log(err.data.details)
+    socket.emit("circle position", position)
+
+
+}
+
+document.addEventListener("mousedown", e => {
+    document.addEventListener("mousemove", drag)
+})
+
+document.addEventListener("mouseup", e => {
+    document.removeEventListener("mousemove", drag)
+})
+
+
+socket.on("move circle", position => {
+    drawCircle(position)
 })
